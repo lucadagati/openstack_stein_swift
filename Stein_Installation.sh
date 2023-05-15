bcd53c7884f9be553823104726c67338750549ae
Luca D'Agati <66645997+lucadagati@users.noreply.github.com>
2023-05-15 23:41:16 +0200

    Update Stein_Installation.sh

--- Stein_Installation.sh
+++ Stein_Installation.sh
@@ -544,21 +544,23 @@ mkdir -p /var/cache/swift
 chown -R root:swift /var/cache/swift
 chmod -R 775 /var/cache/swift
 
+device_name=$(basename "$object_storage_disk")
+
 #account ring creation
 swift-ring-builder account.builder create 10 1 1
-swift-ring-builder account.builder add --region 1 --zone 1 --ip $ip --port 6202 --device $object_storage_disk --weight 100
+swift-ring-builder account.builder add --region 1 --zone 1 --ip $ip --port 6202 --device $device_name --weight 100
 swift-ring-builder account.builder
 swift-ring-builder account.builder rebalance
 
 #container ring creation
 swift-ring-builder container.builder create 10 1 1
-swift-ring-builder container.builder add --region 1 --zone 1 --ip $ip --port 6201 --device $object_storage_disk --weight 100
+swift-ring-builder container.builder add --region 1 --zone 1 --ip $ip --port 6201 --device $device_name --weight 100
 swift-ring-builder container.builder
 swift-ring-builder container.builder rebalance
 
 #object ring creation
 swift-ring-builder object.builder create 10 1 1
-swift-ring-builder object.builder add --region 1 --zone 1 --ip $ip --port 6200 --device $object_storage_disk --weight 100
+swift-ring-builder object.builder add --region 1 --zone 1 --ip $ip --port 6200 --device $device_name --weight 100
 swift-ring-builder object.builder
 swift-ring-builder object.builder rebalance
 
